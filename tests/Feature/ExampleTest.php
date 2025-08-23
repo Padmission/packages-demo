252389c293bcd618bb7539c376e302d1ae5dbd65
manukminasyan <manuk.minasyan1@gmail.com>
2025-08-23 16:22:49 +0400

    Introduce Pest testing framework and refactor example tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,20 +1,7 @@
 <?php
 
-namespace Tests\Feature;
+test('example', function () {
+    $response = $this->get('/login');
 
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
