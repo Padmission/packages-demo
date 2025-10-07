767a6b7e04b66c372fb6de9ed4e9e6a9626e5919
manukminasyan <manuk.minasyan1@gmail.com>
2025-10-07 20:54:24 +0400

    feat: remove Laravel Horizon from dependencies and service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\Filament\AppPanelProvider;
-use App\Providers\HorizonServiceProvider;
 
 return [
     AppServiceProvider::class,
-//    HorizonServiceProvider::class,
     AppPanelProvider::class,
     AdminPanelProvider::class,
 ];
