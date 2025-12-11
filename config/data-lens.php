062322644d0865d87028bc365600182b2e3917b0
manukminasyan <manuk.minasyan1@gmail.com>
2025-12-11 05:12:37 +0400

    feat: enable custom fields integration in data lens

--- config/data-lens.php
+++ config/data-lens.php
@@ -285,7 +285,7 @@ return [
 
     // Third-party integrations
     'integrations' => [
-        'custom_fields' => false,
+        'custom_fields' => true,
         'advanced_tables' => false,
     ],
 
